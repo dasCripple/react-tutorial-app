7b937109118a78f8e0cef1f3bd12286f054152bf
iAM7ander <iAM7ander@outlook.com>
2024-08-27 13:01:15 -0400

    added food component

--- react-tutorial-app/src/App.jsx
+++ react-tutorial-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer";
+import Food from "./Food";
 
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Food></Food>
       <Footer></Footer>
     </>
   );
